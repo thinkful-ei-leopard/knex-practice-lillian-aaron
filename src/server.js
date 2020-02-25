e5c33f8abba92b78b43c0423d07209939630e2ee
Lillian Burnside <lillian.burnside@gmail.com>
2020-02-25 10:33:59 -0600

    real first commit

--- src/server.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-const app = require('./app');
-const { PORT, NODE_ENV } = require('./config');
-
-app.listen(PORT, () => {
-  console.log(`Server is running in ${NODE_ENV} mode on ${PORT}`);
-});
