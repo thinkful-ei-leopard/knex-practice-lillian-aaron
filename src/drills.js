3230f433ea2c7249f5f41dc62250bfa2236fb492
Lillian Burnside <lillian.burnside@gmail.com>
2020-02-25 12:36:57 -0600

    drills 1-3

---
+++ src/drills.js
@@ -0,0 +1,52 @@
+'use strict';
+require('dotenv').config();
+const knex = require('knex');
+
+const knexInstance = knex({
+  client: 'pg',
+  connection: process.env.DB_URL,
+});
+
+// function getItemsWithText(searchTerm){
+//   knexInstance
+//     .select('*')
+//     .from('shopping_list')
+//     .where('name', 'ILIKE', `%${searchTerm}%`)
+//     .then (result => {
+//       console.log(result);
+//     });
+// }
+
+// getItemsWithText('tofu');
+
+// function getAllPaginated(pageNumber) {
+//   const resultsPerPage = 6;
+//   const offset = resultsPerPage * (pageNumber -1);
+//   knexInstance
+//     .select('*')
+//     .from('shopping_list')
+//     .limit(resultsPerPage)
+//     .offset(offset)
+//     .then(result => {
+//       console.log(result);
+//     });
+// }
+
+// getAllPaginated(1);
+
+function getAllAfterDate(daysAgo) {
+  knexInstance
+    .select('name', 'price', 'date_added')
+    .from('shopping_list')
+    .where(
+      'date_added',
+      '<',
+      // eslint-disable-next-line quotes
+      knexInstance.raw(`now() - '?? days'::INTERVAL`, daysAgo)
+    )
+    .then(results => {
+      console.log(results);
+    });
+}
+
+getAllAfterDate(12);
